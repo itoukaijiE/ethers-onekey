b8dd66e260dd5bb385bf61e9389cf3b27c41954e
itoukaiji <itoukai@tutanota.com>
2021-04-10 22:47:46 +0800

    delete unused file

--- src/one.ts
+++
@@ -1,9 +0,0 @@
-import { OnekeySigner } from './index';
-import { ethers, providers } from 'ethers';
-
-(async () => {
-    console.log('aaa');
-    let x = undefined;
-    let b = x || '1';
-    console.log(b);
-})();
